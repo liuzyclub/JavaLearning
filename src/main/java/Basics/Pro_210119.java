0ec80e565cfbec37a4a090d378f0b45803f51ec9
liuzyclub <liuzy74521@163.com>
2021-01-18 15:14:14 +0800

    up

---
+++ src/main/java/Basics/Pro_210119.java
@@ -0,0 +1,28 @@
+package Basics;
+
+public class Pro_210119 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 判断101-200之间有多少个素数，并输出所有素数。
+     *
+     * 提示：素数又叫质数，就是除了1和它本身之外，再也没有整数能被它整除的数。也就是素数只有两个因子。
+     */
+    public static void q1(){
+
+    }
+
+    /**
+     *  输入两个正整数m和n，求其最大公约数和最小公倍数。
+     * @param m
+     * @param n
+     *
+     * 提示：
+     *      概念：https://www.lexiangba.net/31175.html
+     */
+    public static void q2(int m,int n){
+
+    }
+}
