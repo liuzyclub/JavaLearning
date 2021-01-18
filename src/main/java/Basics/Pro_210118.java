91007c178a4e280a4c724f9b820c63b7761de7b6
liuzyclub <liuzy74521@163.com>
2021-01-18 14:53:14 +0800

    up

---
+++ src/main/java/Basics/Pro_210118.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Pro_210118 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *題目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一
+     *对兔子，假如兔子都不死，问每个月的兔子总数为多少？
+     * @param num 参数，为第几个月
+     * @return 返回内容应为兔子数量
+     */
+    public static int q1(int num){
+        return 0;
+    }
+}
